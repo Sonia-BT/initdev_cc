3fbdaab6e7d00915a4b16e21036cfb68668db5a6
Sonia-BT <boutaghousonia11@gmail.com>
2018-04-05 22:11:06 +0200

    first step

--- projet1.sh
+++ projet1.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 rep="initdev"
-
+#création du répértoire
 mkdir $rep
+
+#création des fichiers dans le répértoire
 mkdir $rep/main
 mkdir $rep/licence
 mkdir $rep/makefile
+
+#test de fonctionnalité
+
+if $0 eq initdev
+then return0;
+else echo "Expected arguments, please check the help : initdev –help"
+     retun1;
+fi
+
+
