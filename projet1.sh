f07cd31e74f6bc3896bb86ac2eeac2dfe56cd076
Sonia-BT <boutaghousonia11@gmail.com>
2018-04-05 10:22:11 +0200

    test

---
+++ projet1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "création du répértoire"
+$(mkdir initdev)
+echo "création des fichier du répértoire"
+$(mkdir initdev/main)
+$(mkdir initdev/LICENSE)
+$(mkdir initdev/Makefile)
+
+
